b7d3be4ffdb45b841792dac69afef48c4e6a5995
Douglas Araújo <araujodgdev@gmail.com>
2024-08-16 19:18:01 +0000

    termina a função de desencriptar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const rawTextField = document.querySelector('#raw-text');
-const encryptBtn = document.querySelector('#main-btn')
-const decryptBtn = document.querySelector('#secondary-btn')
+const encryptBtn = document.querySelector('#main-btn');
+const decryptBtn = document.querySelector('#secondary-btn');
+const outputContainer = document.querySelector('#output-container');
 
 let rawText = '';
 
@@ -16,7 +17,7 @@ function encrypt(msg) {
 }
 
 function decrypt(msg) {
-    let decrypted = msg
+  let decrypted = msg
     .replaceAll('ai', 'a')
     .replaceAll('enter', 'e')
     .replaceAll('imes', 'i')
@@ -26,12 +27,31 @@ function decrypt(msg) {
   return decrypted;
 }
 
-rawTextField.addEventListener('input', () => rawText = rawTextField.value);
+rawTextField.addEventListener('input', () => (rawText = rawTextField.value));
 
 encryptBtn.addEventListener('click', () => {
-    document.querySelector('#no-msg-container').setAttribute('style', 'display: none')
-    document.querySelector('#output-container').textContent = encrypt(rawText);
-
+  document
+    .querySelector('#no-msg-container')
+    .setAttribute('style', 'display: none');
+  outputContainer.textContent = encrypt(rawText);
 });
-decryptBtn.addEventListener('click', () => console.log(decrypt(rawText)));
 
+outputContainer.addEventListener('click', () => {
+  let content = outputContainer.textContent;
+
+  const outputRange = document.createRange(); // instancia um objeto range
+  outputRange.selectNodeContents(outputContainer); // define o intervalo do range para cobrir todo o parágrafo do container de output
+
+  const selection = window.getSelection(); // obtém o objeto de seleção atual do navegador
+  selection.removeAllRanges(); // remove as seleções já existentes para evitar múltiplas seleções
+  selection.addRange(outputRange); //adiciona o range criado na seleção
+
+  navigator.clipboard.writeText(content).then(() => {
+    window.alert('Texto copiado para a área de transferência!');
+  });
+});
+
+decryptBtn.addEventListener('click', () => {
+  document.querySelector('#no-msg-container').removeAttribute('style');
+  outputContainer.setAttribute('style', 'display: none');
+});
